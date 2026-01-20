fe3b67d34da90ee87a985c599b4b16bf5216725e
Furn <furnacefurn@gmail.com>
2026-01-20 13:23:47 +0200

    Set up calculator structiure_1

--- python calculator.py
+++ python calculator.py
@@ -3,4 +3,20 @@ from division import divide
 from subtraction import subtraction
 
 
+def main():
+    print("Welcome to Calculator!")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    choice = input("Chooose operation (+, -, *, /): ")
 
+    if choice == '+':
+        # Call the function from addition.py
+        result = add(num1, num2)
+    elif choice == '-':
+        result = subtraction(num1, num2)
+    
+    
+    print(f"The result is: {result}")
+
+if __name__ == "__main__":
+    main()
